669748e829527fb999d1e7eb99a480d1cfe156c0
torresender5 <torresender5@gmail.com>
2021-12-11 11:41:06 -0400

    companyView

--- app/companies/views.py
+++ app/companies/views.py
@@ -3,12 +3,16 @@ from django.views import View
 from django.http import JsonResponse 
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+import json 
 
 # Create your views here.
 class Company(View):
+    
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     
     def get(self, request):
-        pass
+        jd= json.loads(request.body)
+        
+        return JsonResponse(jd)
