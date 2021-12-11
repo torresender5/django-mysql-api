06e77a307c7f0417ad90bdec799e3237e23a0eaf
torresender5 <torresender5@gmail.com>
2021-12-11 11:32:57 -0400

    view

--- company/views.py
+++ company/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+class Company(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        pass
